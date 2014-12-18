40ac704a67e3971ee43d5fc7d8acbe5913fe94b0
Andras Tim <andras.tim@gmail.com>
2014-12-18 15:23:45 +0100

    web/flask-microblog: small fixes

--- web/flask-microblog/setup.sh
+++ web/flask-microblog/setup.sh
@@ -3,9 +3,13 @@
 # Update system
 sudo apt-get install build-essential python3 python-virtualenv
 
-
 # Prepare virtual environment
-virtualenv -p /usr/bin/python3 flask
+if [ ! -e flask ]; then
+    virtualenv -p /usr/bin/python3.4 flask
+fi
+flask/bin/pip install pip --upgrade
+flask/bin/pip install setuptools --upgrade
+
 flask/bin/pip install flask
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
