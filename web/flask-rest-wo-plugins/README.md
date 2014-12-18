e70d616340926da88672196af3dc89b291a2bfb9
Andras Tim <andras.tim@gmail.com>
2014-12-18 15:27:41 +0100

    web/flask-rest-wo-plugins: Initial commit

---
+++ web/flask-rest-wo-plugins/README.md
@@ -0,0 +1,3 @@
+# miguelgrinberg.com
+## Designing a RESTful API with Python and Flask
+http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
---
+++ web/flask-rest-wo-plugins/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update system
+sudo apt-get install build-essential python3 python-virtualenv
+
+# Prepare virtual environment
+if [ ! -e flask ]; then
+    virtualenv -p /usr/bin/python3.4 flask
+fi
+flask/bin/pip install pip --upgrade
+flask/bin/pip install setuptools --upgrade
+flask/bin/pip install flask
