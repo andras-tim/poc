24bc18622ad37fea65b82438ab33a9e72883aed0
Andras Tim <andras.tim@gmail.com>
2015-01-06 03:48:13 +0100

    web/flask-angular-blog: Updated to Step 1

---
+++ web/flask-angular-blog/server/app/forms.py
@@ -0,0 +1,29 @@
+from flask.ext.wtf import Form
+from wtforms_alchemy import model_form_factory
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from .server import db
+from .models import User, Post
+ 
+BaseModelForm = model_form_factory(Form)
+
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(cls):
+        return db.session
+
+
+class UserCreateForm(ModelForm):
+    class Meta:
+        model = User
+
+
+class SessionCreateForm(Form):
+    email = StringField('email', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
---
+++ web/flask-angular-blog/server/app/serializers.py
@@ -0,0 +1,13 @@
+from marshmallow import Serializer, fields
+
+
+class UserSerializer(Serializer):
+    class Meta:
+        fields = ("id", "email")
+
+
+class PostSerializer(Serializer):
+    user = fields.Nested(UserSerializer)
+ 
+    class Meta:
+        fields = ("id", "title", "body", "user", "created_at")
