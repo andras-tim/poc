1d066e5ef7fcb5f4ceb1a9dd67f33bdc56ad0b91
Andras Tim <andras.tim@gmail.com>
2015-01-11 11:51:16 +0100

    web/angular-strap: Initial commit of SQL

---
+++ web/angular-strap/server/app/models.py
@@ -0,0 +1,33 @@
+from flask import g
+from wtforms.validators import Email
+from .server import db, bcrypt
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False, info={'validators': Email()})
+    password = db.Column(db.String(80), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
+ 
+    def __init__(self, email, password):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+ 
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+ 
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+        self.user_id = g.user.id
+ 
+    def __repr__(self):
+        return '<Task %r>' % self.title
--- web/angular-strap/server/app/server.py
+++ web/angular-strap/server/app/server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
 
 from .config import Config
 
@@ -8,6 +10,12 @@ config = Config.read()
 app = Flask(__name__, static_url_path='/%s' % config.App.NAME)
 app.config.update(config["Flask"])
 # import pprint; app.logger.debug("Flask config:\n%s" % pprint.pformat(app.config))
+
+# flask-sqlalchemy
+db = SQLAlchemy(app)
+
+# flask-bcrypt
+bcrypt = Bcrypt(app)
 
 
 # Init models and views (must be the last)
