7783386c3a454d0098733f4cd0807321d05198dc
Andras Tim <andras.tim@gmail.com>
2015-01-14 15:11:27 +0100

    web/angular-strap: Debug performance problems
    Command:
     * flask/bin/kernprof -l db_create.py
     * flask/bin/python -m line_profiler db_create.py.lprof

--- web/angular-strap/server/app/server.py
+++ web/angular-strap/server/app/server.py
@@ -7,25 +7,39 @@ from flask.ext.bcrypt import Bcrypt
 from .config import Config
 
 
-config = Config.read()
+config = None
+app = None
+db = None
+lm = None
+api = None
+bcrypt = None
 
-app = Flask(__name__, static_url_path='/%s' % config.App.NAME)
-app.config.update(config["Flask"])
-# import pprint; app.logger.debug("Flask config:\n%s" % pprint.pformat(app.config))
 
-# flask-sqlalchemy
-db = SQLAlchemy(app)
+@profile
+def start():
+    global config, app, db, lm, api, bcrypt
 
-# flask-login
-lm = LoginManager()
-lm.init_app(app)
+    config = Config.read()
 
-# flask-restful
-api = restful.Api(app)
+    app = Flask(__name__, static_url_path='/%s' % config.App.NAME)
+    app.config.update(config["Flask"])
+    # import pprint; app.logger.debug("Flask config:\n%s" % pprint.pformat(app.config))
 
-# flask-bcrypt
-bcrypt = Bcrypt(app)
+    # flask-sqlalchemy
+    db = SQLAlchemy(app)
 
+    # flask-login
+    lm = LoginManager()
+    lm.init_app(app)
+
+    # flask-restful
+    api = restful.Api(app)
+
+    # flask-bcrypt
+    bcrypt = Bcrypt(app)
+
+start()
 
 # Init models and views (must be the last)
 from app import views
+
